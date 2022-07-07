f8402a435aa1f77f0334adcabf3155c1aed80662
danagt <85912881+danagt@users.noreply.github.com>
2022-07-07 10:56:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// btn = ""
+// btn.onclick = () => {
+//     fetch("https://api.github.com/users/" + champ.value)
+//         .then(response => response.json())
+//         .then(data => {
+//             resultado.textContent = "";
+//             resultado.textContent = `Cuenta de ${data.name}`;
+//             const img = document.createElement("img");
+//             img.src = data.avatar_url;
+//             resultado.appendChild(img);
+
+//     })
+// }
+
+
+fetch("https://api.github.com/users/bdebon") // usuarios para probar: nedfri, bdebon 
+    .then(response => response.json())
+    .then(data => console.table(data))
